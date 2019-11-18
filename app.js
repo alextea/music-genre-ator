2c2819caa06cea91c3b6d70cfd72a0749753452a
Alex Torrance <alex.torrance@evenlydistributed.xyz>
2019-11-18 09:15:59 +0000

    Store each unique genre in the database and retrieve using slug 💽
    * Set up some rudimentary error handling
    *

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express     = require('express');
 var nunjucks    = require('nunjucks');
 var path        = require('path');
 var screenshot  = require('./utils/screenshot');
+var database    = require('./utils/database_utils');
+var words       = require('./data/words.json');
 var app         = express();
 
 nunjucks.configure('views', {
@@ -16,9 +18,17 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use('/styles', express.static(path.join(__dirname, 'public/styles')));
 
-var words = require('./words.json');
 
-var getRandomWord = function(array) {
+if (process.env.NODE_ENV == "production") {
+  var siteUrl = "https://musicgenre.site";
+} else {
+  var siteUrl = "http://localhost:3000";
+}
+
+console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV);
+console.log('siteUrl = ' + siteUrl)
+
+function getRandomWord(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
@@ -86,54 +96,109 @@ function buildUrlQueryString(url, query) {
   return url;
 }
 
-app.get('/', function (req, res) {
-  var genre = generateGenre();
-  console.log(genre);
+function makeTwitterShareUrl(genre, slug) {
+  var twitterUrl = "https://twitter.com/intent/tweet";
+  var twitterQuery = {
+    text: "My new favourite genre is " + genre,
+    url: siteUrl + "/" + slug,
+    hashtags: "musicgenreator",
+    via: "alex_tea"
+  }
 
-  var slug = textToSlug(genre);
+  return buildUrlQueryString(twitterUrl, twitterQuery)
+}
 
-  res.redirect('/'+slug);
-});
+function makeFacebookShareUrl(genre, slug) {
+  var faceBookUrl = "https://www.facebook.com/dialog/share?";
+  var faceBookQuery = {
+    href: siteUrl + "/" + slug,
+    display: "page",
+    redirect_uri: siteUrl
+  }
+
+  return buildUrlQueryString(faceBookUrl, faceBookQuery);
+}
 
 app.get('/screenshot/:genre', function (req, res) {
   res.render('screenshot.html', { genre: req.params.genre });
 })
 
-app.get('/:genre', function (req, res) {
-  var genre = req.params.genre;
-
-  var twitterUrl = "https://twitter.com/intent/tweet";
-  var twitterQuery = {
-    text: "My new favourite genre is " + genre,
-    url: "https://musicgenre.site",
-    hashtags: "musicgenreator",
-    via: "alex_tea"
-  }
-
-  var twitterShareLink = buildUrlQueryString(twitterUrl, twitterQuery);
-
-  var faceBookUrl = "https://www.facebook.com/dialog/share?";
-
-  var faceBookQuery = {
-    href: "https://musicgenre.site",
-    display: "page",
-    redirect_uri: "ttps://musicgenre.site"
-  }
-
-  var faceBookShareLink = buildUrlQueryString(faceBookUrl, faceBookQuery);
-
-  // capture screenshot
-  screenshot
-    .getScreenShot('http://localhost:3000/screenshot/'+genre)
-    .catch(err => reject('Screenshot failed'))
-
-  res.render('index.html',
-    {
-      genre: genre,
-      twitter_share_link: twitterShareLink,
-      facebook_share_link: faceBookShareLink
-    });
+app.get('/favicon.ico', function(req, res, next) {
+  res.sendStatus(404);
 })
+
+app.get('/', function (req, res, next) {
+  var genre = generateGenre();
+  var slug = textToSlug(genre);
+
+  // check if genre exists in database
+  database.getGenre(slug)
+    .then(function(data) {
+      if (!data) {
+        console.log(slug + ' not found in db');
+        // add to database
+        database.addGenre(genre, slug);
+
+        // capture screenshot
+        screenshot
+          .getScreenShot(siteUrl + '/screenshot/', slug)
+          .catch(err => reject('Screenshot failed'))
+
+        var twitterShareLink = makeTwitterShareUrl(genre, slug);
+        var faceBookShareLink = makeFacebookShareUrl(genre, slug);
+
+        res.render('index.html',
+          {
+            slug: slug,
+            genre: genre,
+            twitter_share_link: twitterShareLink,
+            facebook_share_link: faceBookShareLink
+        });
+      }
+    })
+    .catch(function(error) {
+      next(error);
+    })
+})
+
+app.get('/:slug', function (req, res, next) {
+  var slug = req.params.slug;
+  console.log("slug = "+slug);
+  database.getGenre(slug)
+    .then(function(data) {
+      if (!data) {
+        var err = new Error("Genre not found");
+        err.status = 404;
+        next(err);
+      } else {
+        console.log(slug + ' exists in db');
+        var genre = data.genre;
+
+        var twitterShareLink = makeTwitterShareUrl(genre, slug);
+        var faceBookShareLink = makeFacebookShareUrl(genre, slug);
+
+        res.render('index.html',
+          {
+            slug: slug,
+            genre: genre,
+            twitter_share_link: twitterShareLink,
+            facebook_share_link: faceBookShareLink
+        });
+      }
+    })
+    .catch(function(error) {
+      next(error);
+    })
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Oops")
+});
+
+app.use(function (err, req, res, next) {
+  console.log(err.stack)
+  res.status(500).send(err.message)
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
@@ -141,7 +206,3 @@ var server = app.listen(3000, function () {
 
   console.log('Music Genre-ator app listening at http://%s:%s', host, port);
 });
-
-for (var i = 0; i < 10; i++) {
-  console.log(generateGenre());
-}
