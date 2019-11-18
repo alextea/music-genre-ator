af56afc7730cc6530ae3521141a9843c09369e6b
Alex Torrance <alex.torrance@evenlydistributed.xyz>
2019-11-18 09:19:15 +0000

    Simplify deploy script 🔌

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,8 @@
 
 set -x
 
-# delete existing deployment
-rm -rf /home/musicgen/apps/music-genre-ator
-
-# copy new deploy
-cp -r  /home/musicgen/repositories/music-genre-ator /home/musicgen/apps
-
 # cd to new direction
-cd /home/musicgen/apps/music-genre-ator
+cd /home/musicgen/repositories/music-genre-ator
 
 npm install
 npm deploy
