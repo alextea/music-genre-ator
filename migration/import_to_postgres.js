5bf9036a1f2e76bf4827c6ffed26d00da41529e1
Alex Torrance <hello@alextorrance.co.uk>
2026-01-03 00:00:43 +0000

    Use public database url to allow migration from local machine

--- migration/import_to_postgres.js
+++ migration/import_to_postgres.js
@@ -8,7 +8,7 @@ const { Pool } = require('pg')
 const fs = require('fs')
 const path = require('path')
 
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_PUBLIC_URL
 
 async function importData() {
   if (!DATABASE_URL) {
